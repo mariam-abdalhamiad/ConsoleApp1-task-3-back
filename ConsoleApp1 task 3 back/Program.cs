4cdd0d60372d5ba259b0769a810cc8cd98abfd6a
mariam-abdalhamiad <mariamabdalhamiad@gmail.com>
2026-03-23 09:08:15 +0200

    Add project files.

---
+++ ConsoleApp1 task 3 back/Program.cs
@@ -0,0 +1,53 @@
+﻿int grade = int.Parse(Console.ReadLine());
+if (grade >= 90 && grade <=100)
+{
+
+    Console.WriteLine("excellent");
+
+}
+else  if (grade >=80 &&  grade < 90)
+{
+    Console.WriteLine("very good");
+
+}
+else if(grade >=  70 && grade < 80)
+{
+
+    Console.WriteLine("good");
+        }
+else if (grade >= 50 && grade < 70)
+{
+
+    Console.WriteLine("pass");
+}
+else { Console.WriteLine("fail"); }
+
+//----------------------------------------------------
+int day = int.Parse(Console.ReadLine());
+switch(day)
+{
+    case 1:
+        Console.WriteLine("saturday"); 
+        break;
+    case 2:
+        Console.WriteLine("sunday");
+        break;
+    case 3:
+        Console.WriteLine("MONDAY");
+        break;
+    case 4:
+        Console.WriteLine("tuesday") ; 
+        break;
+    case 5:
+        Console.WriteLine("wednesday");
+        break;
+    case 6:
+        Console.WriteLine("thursday");
+        break;
+    case 7:
+        Console.WriteLine("friday");
+        break;
+    default:
+        Console.WriteLine("invaliad number");
+        break;
+            }
